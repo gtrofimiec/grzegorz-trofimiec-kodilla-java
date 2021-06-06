009f2362df7b4762a89bb0799bd1a218586f92d8
gtrofimiec <gtrofimiec@gmail.com>
2021-06-06 09:32:43 +0200

    (13.4) Zadanie: wyzwanie - Food2Door (poprawka 4)

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Food2Door/OrderingDetailsRetriever.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Food2Door/OrderingDetailsRetriever.java
@@ -3,13 +3,10 @@ package com.kodilla.good.patterns.challenges.Food2Door;
 public class OrderingDetailsRetriever {
 
     public OrderingDetails retrieve() {
-        HealthyShop healthyShop = new HealthyShop();
-        ExtraFoodShop extraFoodShop = new ExtraFoodShop();
-        GlutenFreeShop glutenFreeShop = new GlutenFreeShop();
 
         Product product1 = new Product("Tomatoes");
         double productQuantity = 3;
 
-        return new OrderingDetails(healthyShop, product1, productQuantity);
+        return new OrderingDetails(new HealthyShop(), product1, productQuantity);
     }
 }
